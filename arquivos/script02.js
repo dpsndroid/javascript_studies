ba46e2c8d954b0045ecdd4d337c445f73fcf97a8
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-08-05 16:00:22 -0300

    Update_aug05

--- arquivos/script02.js
+++ arquivos/script02.js
@@ -41,3 +41,33 @@ function verificar() {
         
     }
 }
+
+function contar(){
+    let inicio = document.getElementById("txti");
+    let fim = document.getElementById("txtf");
+    let passo = document.getElementById("txtp");
+    let res = document.getElementById("res");
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = `Impossível realizar a conta. Faltam parâmetros`;
+        
+    } else {
+        res.innerHTML = "Contando:  ";
+        let i = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+    if(p<=0){
+        window.alert(`Passo inválido. Consiserando passo = 1`);
+        p = 1;
+    }
+    
+    if(i<f){ // contagem crescente
+        for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c}  \u{1F449}   `; //formatação unicode para emoji no JS
+    }
+    } else { // contagem decrescente
+        for(let c = i; c>= f; c-= p){
+            res.innerHTML += `${c}   \u{1F449}    `;
+        }
+    }
+    }
+}

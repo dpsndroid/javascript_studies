65864d3cbdc5c9aed316c701d7c0bfb3a0183747
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-10-01 19:40:17 -0300

    update_oct01

---
+++ app.js
@@ -0,0 +1,30 @@
+const Kelvin = 313;
+// Declaração da variável kelvin
+
+const celsius = Kelvin - 273;
+// Declaração da variável Celsius
+
+let fahrenheit = celsius * (9/5)+32;
+// Declaração da variável fahrenheit
+
+let newton = celsius * (33/100);
+newton = Math.floor(newton);
+
+fahreinheit = Math.floor(fahrenheit);
+// resultado arredondado para baixo
+
+console.log(`The temperature is ${fahreinheit} degrees Fahreinheit.`);
+
+console.log(`The temperature is ${newton} degrees Newton`);
+
+console.log(`The temperature is ${celsius} degrees Celsius`);
+
+
+
+
+
+
+
+
+
+
---
+++ app2.js
@@ -0,0 +1,29 @@
+const myAge = 14;
+// idade
+
+let earlyYears = 2;
+// anos iniciais
+
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+// anos posteriores
+
+laterYears *= 4;
+// posteriores vezes 4
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+// minha idade cachorro
+
+let myName = "Daniel Nepomuceno".toLowerCase();
+// letras minúsculas
+
+console.log(`My name is ${myName}, I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+

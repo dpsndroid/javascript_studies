f0b33e1b55ad48005276910f349502230c614d62
Daniel Nepomuceno <105947140+dpsndroid@users.noreply.github.com>
2022-10-31 18:41:03 -0300

    Create magic8ball.js

---
+++ magic8ball.js
@@ -0,0 +1,47 @@
+let userName = "Daniel";
+
+userName ? console.log(`Hello, ${userName}!`): console.log(`Hello!`);
+
+const userQuestion = "Am I going to win a prize?"
+
+console.log(`${userName} has asked ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8 );
+
+console.log(randomNumber);
+
+let eightBall = "";
+
+switch (randomNumber) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+  case 1:
+    eightBall = "It is decidely so";
+    break;
+  case 2:
+    eightBall = "Reply hazy try again";
+    break;
+  case 3:
+    eightBall = "Cannot predict now";
+    break;
+  case 4:
+    eightBall = "Do not count on it";
+    break;
+  case 5:
+    eightBall = "My sorces say no";
+    break;
+  case 6:
+    eightBall = "Outlook not so good";
+    break;
+  case 7:
+    eightBall = "Signs point to yes";
+    break;
+}
+
+console.log(`The Magic Eight Ball says, ${eightBall}.`);
+
+
+
+
+
